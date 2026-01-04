159aaf5c0fdbf5a3dc8b020633e941dfb5d6b8e7
Bloszom <blossom.giane@decagon.dev>
2026-01-04 01:55:47 +0100

    addition of profile page to push to git repo

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -10,42 +10,91 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   TextEditingController controller = TextEditingController();
   bool? isChecked = false;
+  bool isSwitched = false;
+  double sliderValue = 0.0;
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          TextField(
-            controller: controller,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+              onEditingComplete: () {
+                setState(() {});
+              }
             ),
-            onEditingComplete: () {
-              setState(() {});
-            }
-          ),
-          Text(controller.text,),
-          Checkbox(
-            tristate: true,
-            value: isChecked,
-            onChanged: (bool? value) {
+            Text(controller.text,),
+            Checkbox.adaptive(
+              tristate: true,
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            CheckboxListTile.adaptive(
+              tristate: true,
+              title: Text("Click me"),
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            Switch.adaptive(
+              value: isSwitched,
+               onChanged: (bool value) {
               setState(() {
-                isChecked = value;
+                isSwitched = value;
               });
-            },
-          ),
-          CheckboxListTile(
-            tristate: true,
-            title: Text("Click me"),
-            value: isChecked,
-            onChanged: (bool? value) {
+            }),
+            SwitchListTile.adaptive(
+              title: Text(" Switch me"),
+              value: isSwitched,
+               onChanged: (bool value) {
               setState(() {
-                isChecked = value;
+                isSwitched = value;
               });
-            },
-          ),
-        ],
+            }),
+            Slider.adaptive(
+              max: 10.0,
+              value: sliderValue,
+              divisions: 10,
+             onChanged: (double value){
+              setState( (){
+                sliderValue = value;
+              });
+              print(value);
+             },
+            ),
+            InkWell(
+              splashColor: Colors.teal,
+              onTap: (){
+                print("Image Tapped");
+              },
+              child:Container(
+                height: 50,
+                width: double.infinity,
+                color: Colors.white12,
+              ) 
+            ),
+            ElevatedButton(onPressed: (){},
+             style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.teal,
+              foregroundColor: Colors.white,
+             ),
+             child: Text("Submit")
+             ),
+          ],
+        ),
       ),
     );
   }
